ce23e13d02a6643effe43d9581b2014e925f18c7
Olivier3456 <olivier.larrey@gmail.com>
2023-01-06 11:27:39 +0100

    Just a test

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    string test;
+    void Start()
+    {
+        test = "TEST";
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
